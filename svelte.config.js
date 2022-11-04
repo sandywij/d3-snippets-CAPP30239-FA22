11528a055fdb877e486e596b33d163daed7e0a00
Sandy Ng <sandywij@gmail.com>
2022-11-03 20:36:32 -0400

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = 'production' === 'development';
+
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : ''
-		}
+		paths: {}
 	}
 };
 
